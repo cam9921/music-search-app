4065a91f6c806b4897d7230aee24c9ba42efa7c8
Camden Taylor <camdentaylor1992@gmail.com>
2019-01-07 21:21:45 -0700

    Many updates: Got a initial login page to work, and to retreive an access token from Spotify. It works now for searching for artists, but I need to get it passed through the rest of the routes, e.g. artist details and album results.

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 const express = require('express');
 const request = require('request');
 const path = require('path');
+const querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+// let access_token = '';
+// let refresh_token = '';
 const oAuthToken = require('./auth/spotifyTokens.js')
 
 //Authentication IDs
-const client_id = '34b5994840a04dca8f147be844040dfb'
-const client_secret = '2be30b23d5484c599f00724807e1d8c1'
+const client_id = '34b5994840a04dca8f147be844040dfb'; // Your client id
+const client_secret = '2be30b23d5484c599f00724807e1d8c1'; // Your secret
+const redirect_uri = 'http://localhost:3000/callback/'; // Your redirect uri
 
 //Init app
 const app = express();
@@ -17,12 +22,66 @@ app.set('view engine', 'pug');
 
 //Add routes
 app.get('/', (req, res) => {
-    res.render('search');
+    res.render('login_prompt'); // Make login prompt template with 
 });
 
+//Add routes
+app.get('/search/:accessToken', (req, res) => {
+    // console.log(req.params.accessToken);
+    const accessToken = req.params.accessToken;
+    res.render('search', {accessToken: accessToken});
+});
+
+// let redirect_uri = 
+//   process.env.REDIRECT_URI || 
+//   'http://localhost:8888/callback'
+
+app.get('/login', function(req, res) {
+  console.log(client_id, redirect_uri);
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+    //   client_id: process.env.SPOTIFY_CLIENT_ID, // Proper way to bring in variables, declare in command line
+      client_id: client_id,
+      scope: 'user-read-private user-read-email',
+      redirect_uri
+    }))
+})
+
+app.get('/callback', function(req, res) {
+  let code = req.query.code || null
+  let authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(
+        // process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET // Again, do this when you declare vars in command line
+        client_id + ':' + client_secret
+        // `${client_id}:${client_secret}`
+      ).toString('base64'))
+    },
+    json: true
+  }
+  console.log(code, authOptions)
+  request.post(authOptions, function(error, response, body) {
+    var access_token = body.access_token
+    console.log(access_token)
+    // let uri = process.env.FRONTEND_URI || 'http://localhost:3000' // The example this code was pulled from redirects to a react app running on port 3000
+    //                                                                  We are going to try to do it all in express.. let's see how it goes!
+    // res.redirect(uri + '?access_token=' + access_token)
+    res.redirect(`/search/${access_token}`)
+  })
+})
+
+
 //Route to search for artists.
-app.get('/results', (req, res) => {
+app.get('/results/access=:accessToken', (req, res) => {
     const query = req.query.search;
+    const accessToken = `Bearer ${req.params.accessToken}`;
 
     //This url should return JSON containing the artist ID, and we can use that to search for more detailed data on the artist. 
     const artistRequestUrl = `https://api.spotify.com/v1/search?q=${query}&type=artist`
@@ -32,7 +91,7 @@ app.get('/results', (req, res) => {
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": oAuthToken
+            "Authorization": accessToken
         }
     };
 
@@ -41,7 +100,12 @@ app.get('/results', (req, res) => {
         if(!error && response.statusCode == 200) {
             const data = JSON.parse(body);
             if(!data["Error"]) {
-                res.render('results', {artists: data.artists.items, error: '', title: query});
+                res.render('results', {
+                    artists: data.artists.items, 
+                    error: '', 
+                    title: query, 
+                    accessToken: accessToken
+                });
              } else {
                 console.log(response);
                 // res.render('results', {data:{}, error: data["Error"]});
@@ -51,15 +115,16 @@ app.get('/results', (req, res) => {
 });
 
 //Route to show artist details
-app.get('/artist/:artistid', (req, res) => {
+app.get('/artist/:artistid/access=:accessToken', (req, res) => {
     const artistID = req.params.artistid;
+    const accessToken = `Bearer ${req.params.accessToken}`;
 
     const artistInfoQuery = {
         url: `https://api.spotify.com/v1/artists?ids=${artistID}`,
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": oAuthToken
+            "Authorization": accessToken
         }
     };
 
@@ -110,11 +175,6 @@ app.get('/albums/:artistid', (req, res) => {
         };
     });
 });
-
-//Authentication token request route.
-// app.post('', () => {
-
-// })
 
 //Start server
 app.listen(3000, () => {
